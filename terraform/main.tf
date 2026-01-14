6ffb35d9b07f096004a41f32de207758314907ae
Nikit <Nikitaskatcm@mail.ru>
2026-01-14 20:01:45 +0300

    Update Terraform config: Add VMs, network, outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,93 @@
-"# Terraform configuration" 
+"# Terraform configuration" # terraform/main.tf
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.89.0"
+    }
+  }
+}
+
+provider "yandex" {
+  cloud_id  = var.yandex_cloud_id
+  folder_id = var.yandex_folder_id
+  zone      = "ru-central1-a"
+}
+
+# Создаем сеть
+resource "yandex_vpc_network" "monitoring_network" {
+  name = "monitoring-network"
+}
+
+# Создаем подсеть
+resource "yandex_vpc_subnet" "monitoring_subnet" {
+  name           = "monitoring-subnet"
+  network_id     = yandex_vpc_network.monitoring_network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  zone           = "ru-central1-a"
+}
+
+# ВМ для мониторинга (Prometheus + Grafana)
+resource "yandex_compute_instance" "monitoring_server" {
+  name        = "monitoring-server"
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8vmcue7aajpmeo39kk" # Ubuntu 22.04
+      size     = 20
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.monitoring_subnet.id
+    nat       = true # Даем внешний IP
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  labels = {
+    role = "monitoring"
+  }
+}
+
+# ВМ - "цель" для мониторинга
+resource "yandex_compute_instance" "target_server" {
+  name        = "target-server"
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8vmcue7aajpmeo39kk" # Ubuntu 22.04
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.monitoring_subnet.id
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  labels = {
+    role = "target"
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,18 @@
-"# Variables" 
+# terraform/variables.tf
+variable "yandex_cloud_id" {
+  description = "Yandex Cloud Cloud ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "yandex_folder_id" {
+  description = "Yandex Cloud Folder ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to SSH public key"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}"# Variables" 
